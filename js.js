0a446638e1731837654766757e2c0d089f2552d4
Romina Dehesa <rominadehesa@gmail.com>
2026-06-19 15:25:28 -0300

    fix

--- js.js
+++ js.js
@@ -80,3 +80,27 @@ document.querySelectorAll(".close").forEach(btn => {
         btn.closest(".popup").style.display = "none";
     });
 });
+
+
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("nav-list");
+
+const menuIcon = `
+<svg width="18" height="14" viewBox="0 0 18 14">
+    <path d="M0 2V0H18V2H0ZM0 14V12H18V14H0ZM0 8V6H18V8H0Z" fill="white"/>
+</svg>
+`;
+
+const closeIcon = `
+<svg width="14" height="14" viewBox="0 0 14 14">
+    <path d="M1.4 14L0 12.6L5.6 7L0 1.4L1.4 0L7 5.6L12.6 0L14 1.4L8.4 7L14 12.6L12.6 14L7 8.4L1.4 14Z" fill="#1F0808"/>
+</svg>
+`;
+
+btn.addEventListener("click", () => {
+    menu.classList.toggle("open");
+    document.querySelector("nav").classList.toggle("open");
+    btn.innerHTML = menu.classList.contains("open")
+        ? closeIcon
+        : menuIcon;
+});
